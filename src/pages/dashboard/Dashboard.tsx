da9a251f5fa31375ee6999f7001aa280d19c6a7a
EfraimCancio <efraimfonseca@gmail.com>
2023-01-10 11:35:29 -0300

    feat: Conclusão do Layout Base da aplicação

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -5,7 +5,7 @@ import { LayoutBaseDePagina } from '../../shared/layouts';
 export const Dashboard = () => {
   return (
     // eslint-disable-next-line react/no-children-prop
-    <LayoutBaseDePagina titulo={'Página Inicial'}>
+    <LayoutBaseDePagina titulo={'Página Inicial'} barraDeFerramentas={<>Barra de ferramentas</>}>
         Testando
     </LayoutBaseDePagina>
   );
--- src/shared/layouts/LayoutBaseDePagina.tsx
+++ src/shared/layouts/LayoutBaseDePagina.tsx
@@ -1,39 +1,49 @@
 //import { PropsWithChildren } from 'react';
 import { Box, Icon, IconButton, Typography, useMediaQuery, useTheme } from '@mui/material';
+import { ReactNode } from 'react';
 import { useDrawerContext } from '../contexts';
 
 interface ILayoutBaseDePagina {
     titulo: string;
     children: string;
+    barraDeFerramentas?: ReactNode;
 }
 
 // eslint-disable-next-line react/prop-types
-export const LayoutBaseDePagina: React.FunctionComponent<ILayoutBaseDePagina> = ( {titulo, children} ) => {
+export const LayoutBaseDePagina: React.FunctionComponent<ILayoutBaseDePagina> = ( {titulo, children, barraDeFerramentas} ) => {
 
   const theme = useTheme();  
   const smDown = useMediaQuery(theme.breakpoints.down ('sm')); 
+  const mdDown = useMediaQuery(theme.breakpoints.down ('md')); 
 
   const { toggleDrawerOpen } = useDrawerContext();
 
   return (
     <Box height="100%" display="flex" flexDirection="column" gap={1}>
-      <Box padding={1} display="flex" alignItems="center" height={theme.spacing(12)} gap={1}>
+      <Box padding={1} display="flex" alignItems="center" gap={1} height={theme.spacing(smDown ? 6 : mdDown ? 8 : 12)}>
         {smDown && (
           <IconButton onClick={toggleDrawerOpen}>
             <Icon>menu</Icon>  
           </IconButton>
         )}
         
-        <Typography variant="h5">
+        <Typography 
+          overflow="hidden"
+          whiteSpace="nowrap"
+          textOverflow="ellipsis"
+          variant={smDown ? 'h5' : mdDown ? 'h4' : 'h3'}
+        >
           {titulo}
         </Typography>
       </Box> 
 
-      <Box>
-        Barra de ferramentas
-      </Box>
+      {barraDeFerramentas && (
+        <Box>
+          {barraDeFerramentas}
+        </Box>
+      )}      
 
-      <Box>
+      <Box flex={1} overflow="auto">
         {children}
       </Box>
     </Box>  
