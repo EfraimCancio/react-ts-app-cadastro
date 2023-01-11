019616ec895d71579550797e5cd11ecefd28d572
EfraimCancio <efraimfonseca@gmail.com>
2023-01-11 16:58:17 -0300

    feat: barra de ferramentas

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,11 +1,17 @@
 
 
+import { BarraDeFerramentas } from '../../shared/components';
 import { LayoutBaseDePagina } from '../../shared/layouts';
 
 export const Dashboard = () => {
   return (
     // eslint-disable-next-line react/no-children-prop
-    <LayoutBaseDePagina titulo={'Página Inicial'} barraDeFerramentas={<>Barra de ferramentas</>}>
+    <LayoutBaseDePagina 
+      titulo={'Página Inicial'} 
+      barraDeFerramentas={(
+        <BarraDeFerramentas />
+      )}
+    >
         Testando
     </LayoutBaseDePagina>
   );
