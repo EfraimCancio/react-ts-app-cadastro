79234a4cdade982d3a4554d867dec7bb3a071391
EfraimCancio <efraimfonseca@gmail.com>
2023-01-18 11:27:55 -0300

    feat: Documentando as variáveis de ambiente

--- src/shared/environments/index.ts
+++ src/shared/environments/index.ts
@@ -3,10 +3,16 @@ export const Environment = {
    * Define a quantidade de linhas à ser carregada por padrão nas listagens
    */
   LIMITE_DE_LINHAS: 10,
-  
+  /**
+   * Placeholder exibido em inputs de busca
+   */
   INPUT_DE_BUSCA: 'Pesquisar...',
-  
+  /**
+   * Texto exibido quando nenhum registro é encontrado na busca
+   */
   LISTAGEM_VAZIA: 'Nenhum registro encontrado.',
-  
+  /**
+   * Url base de consulta dos dados dessa aplicação
+   */
   URL_BASE:'http://localhost:3333'
 };
